28729c0443d39dcd514586e871b57539c903e361
israeldch <israeldch@hotmail.com>
2014-06-10 17:55:19 -0500

    Index agregado

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php session_start(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml2/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<link rel="shortcut icon" href="favicon_dgb.ico" type="image/x-icon">
+<noscript><meta http-equiv="refresh" content="0; URL=Indexj.php"/></noscript>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>* * * SISTEMA DE SUSCRIPCIONES * * *</title>
+  <style type="text/css"></style>
+<link rel="stylesheet" type="text/css" href="formato.css" />
+</head>
+
+<body >
+<? include ("encabezadoIndex.php");?>
+<br><br>
+<table border="0" >
+<tr><td align="left">
+<ul>
+ 	<li><a href="http://132.248.9.195/web/suscripciones/Listado.php"><b class="grande">Listado de Suscripciones 2014</b></a></li><br>
+	<li><a href="http://132.248.9.195/web/suscripciones/sistemaRenovaciones.php"><b class="grande">M&oacute;dulo de Renovaci&oacute;n 2015</b></a></li><br>
+	
+</ul>
+</td></tr>
+</table>
+<br><br>
+<?include("pie.php");?>
+</body>
+</html>
